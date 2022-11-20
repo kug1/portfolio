a377e2be0b299f677cbbf5fa6fe7beb1a54c1467
kug1 <bahadirrcakir61@gmail.com>
2022-11-20 20:03:05 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <dark-mode v-motion-slide-right></dark-mode>
+  <header v-motion-slide-top>
+    <the-header></the-header>
+  </header>
+  <main v-motion-slide-top>
+    <projects-grid></projects-grid>
+  </main>
+  <footer v-motion-slide-top>
+    <the-footer></the-footer>
+  </footer>
+</template>
+
+<script>
+import TheHeader from "./components/layouts/TheHeader.vue";
+import TheFooter from "./components/layouts/TheFooter.vue";
+
+import DarkMode from "./components/DarkMode.vue";
+import ProjectsGrid from "./components/ProjectsGrid.vue";
+
+export default {
+  components: {
+    DarkMode,
+    TheHeader,
+    TheFooter,
+    ProjectsGrid,
+  },
+};
+</script>
+
+<style>
+html.dark {
+  color-scheme: dark;
+}
+
+body {
+  transition: all 0.4s cubic-bezier(0.075, 0.82, 0.165, 1);
+  @apply bg-[#ffffff] text-[#0b0f10] dark:bg-[#0b0f10] dark:text-white;
+}
+</style>
---
+++ src/components/ProjectItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    class="rounded border-2 border-black dark:border-white flex flex-col p-5 overflow-hidden w-[21.50rem]"
+  >
+    <div class="flex flex-row flex-wrap justify-between content-center h-6">
+      <a
+        class="flex flex-row text-blue-600 dark:text-github-blue"
+        :href="url"
+        target="_blank"
+      >
+        <icon-svg
+          icon="ri:link-m"
+          width="16"
+          class="fill-blue-600 mr-1 self-center"
+        />
+        <h1 class="text-center font-semibold">{{ name }}</h1>
+      </a>
+      <div class="flex flex-row items-center">
+        <span class="p-1 text-sm font-medium">{{ stars }}</span>
+      </div>
+      <icon-svg icon="mdi:star" />
+    </div>
+    <div class="h-full flex flex-col justify-between">
+      <p class="text-sm font-thin">{{ desc }}</p>
+      <div class="flex flex-row mt-2">
+        <icon-svg :icon="decideIcon" width="20" :inline="true" />
+        <span class="text-sm font-medium ml-1">{{ language }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Icon } from "@iconify/vue";
+
+export default {
+  data() {
+    return {};
+  },
+  props: ["name", "url", "desc", "stars", "language"],
+  components: {
+    IconSvg: Icon,
+  },
+  computed: {
+    decideIcon() {
+      const lang = this.language.toLowerCase();
+
+      switch (lang) {
+        case "vuejs":
+          return "mdi:vuejs";
+      }
+      return "mdi:language-" + lang;
+    },
+  },
+};
+</script>
