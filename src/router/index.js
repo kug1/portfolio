555fc96c48ea7c3121a2f9b5dc3e734827452be7
kug1 <bahadirrcakir61@gmail.com>
2022-12-04 15:22:15 +0300

    router

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router';
+// Components
+import TheHeader from '../components/layouts/TheHeader.vue';
+import About from '../components/About.vue';
+import ProjectsPage from '../components/ProjectsPage.vue';
+import ProjectsGrid from '../components/ProjectsGrid.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/portfolio/',
+      components: {
+        header: TheHeader,
+        about: About,
+        projects: ProjectsGrid,
+      },
+      meta: { transition: 'slide-left' },
+    },
+    {
+      name: 'projects',
+      path: '/portfolio/projects',
+      components: { projects: ProjectsPage },
+      meta: { transition: 'slide-right' },
+    },
+    // { path: '/:notFound(.*)', component: NotFound } // Just learned that github has a default 404 page. No need to make our own here.
+  ],
+});
+
+export default router;
