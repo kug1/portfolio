2686b4f2c614b4e2b6997c7aa6c53051e3c334a3
kug1 <bahadirrcakir61@gmail.com>
2022-12-04 15:45:23 +0300

    fixed mobile style overflow

--- src/components/ProjectsPage.vue
+++ src/components/ProjectsPage.vue
@@ -1,8 +1,8 @@
 <template>
-  <section class="grid place-content-center">
+  <section class="grid place-content-center overflow-hidden">
     <h1 class="header-mid mt-24">All Projects</h1>
     <div
-      class="max-w-fit flex flex-wrap mt-12 lg:mt-24 justify-center align-center"
+      class="max-w-fit flex flex-wrap mt-12 lg:mt-24 justify-center align-center self-center"
     >
       <project-cards></project-cards>
     </div>
