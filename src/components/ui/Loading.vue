c35c724c85868ab7ba6e0b44bde9cebff0f3423c
kug1 <bahadirrcakir61@gmail.com>
2022-12-07 21:07:19 +0300

    made loading icon bigger

--- src/components/ui/Loading.vue
+++ src/components/ui/Loading.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="self-center grid place-content-center py-24">
     <span class="grid place-content-center">
-      <Icon icon="line-md:loading-loop" />
+      <Icon icon="line-md:loading-loop" width="24" />
     </span>
   </div>
 </template>
