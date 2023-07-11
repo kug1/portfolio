ea75821c3d683bbae3d6a38ab9c8da64710fd259
kug1 <ken1.d3v@gmail.com>
2023-07-11 10:21:09 +0300

    added Nuxt to AboutLanguages

--- components/AboutLanguages.tsx
+++ components/AboutLanguages.tsx
@@ -1,6 +1,7 @@
 import Vue from "$tabler-icons/brand-vue.tsx";
 import TypeScript from "$tabler-icons/brand-typescript.tsx";
 import TailwindCSS from "$tabler-icons/brand-tailwind.tsx";
+import IconBrandNuxt from "$tabler-icons/brand-nuxt.tsx";
 
 export default function AboutLanguages() {
   return (
@@ -22,6 +23,12 @@ export default function AboutLanguages() {
           <TailwindCSS class="w-6 h-6" />
           <span class="font-semibold text-sm mx-1 grid place-content-center">
             Tailwind
+          </span>
+        </li>
+        <li class="p-5 m-3 border-2 border-[#000000] rounded flex flex-row justify-start w-[10rem] md:w-[12rem]">
+          <IconBrandNuxt />
+          <span class="font-semibold text-sm mx-1 grid place-content-center">
+            Nuxt
           </span>
         </li>
       </ul>
